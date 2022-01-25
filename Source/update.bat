363206ac950cea76c2cc16f215a3a1e609214d0e
Brad Sanders <thebradleysanders@users.noreply.github.com>
2022-01-25 11:43:31 -0600

    Update update.bat

--- Source/update.bat
+++ Source/update.bat
@@ -5,14 +5,12 @@ set update_url=%1
 IF "%1"=="" (
     set update_url="https://github.com/OpenRMM/Agent.git"
 )
-py OpenRMM.py stop
 CD C:\OpenRMM\
 "C:\Program Files\Git\bin\git" clone %update_url% temp
-py OpenRMM.py stop
 robocopy /XN temp\source\ Agent\
 
 CD C:\OpenRMM\Agent\Py
 py OpenRMM.py update
-py OpenRMM.py start
+py OpenRMM.py restart
 ::start py OpenRMM.py debug
 pause
