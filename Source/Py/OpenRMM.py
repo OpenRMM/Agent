6dd0db297ecac575f421015604f776b1618dc98f
Brad Sanders <thebradleysanders@users.noreply.github.com>
2022-01-26 14:07:26 -0600

    Update version #

--- Source/Py/OpenRMM.py
+++ Source/Py/OpenRMM.py
@@ -34,7 +34,7 @@ Service_Name = "OpenRMMAgent"
 Service_Display_Name = "OpenRMM Agent"
 Service_Description = "A free open-source remote monitoring & management tool."
 
-Agent_Version = "2.2.0"
+Agent_Version = "2.2.1"
 
 LOG_File = "C:\OpenRMM\Agent\Agent.log"
 DEBUG = False
