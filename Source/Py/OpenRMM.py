160a89ec13775604439c7a307ef468796c671cbe
Brad Sanders <thebradleysanders@users.noreply.github.com>
2022-01-28 08:26:28 -0600

    Fix Update Script

--- Source/Py/OpenRMM.py
+++ Source/Py/OpenRMM.py
@@ -33,7 +33,7 @@ Service_Name = "OpenRMMAgent"
 Service_Display_Name = "OpenRMM Agent"
 Service_Description = "A free open-source remote monitoring & management tool."
 
-Agent_Version = "2.2.5"
+Agent_Version = "2.2.6"
 
 LOG_File = "C:\OpenRMM\Agent\Agent.log"
 DEBUG = False
--- Source/update.bat
+++ Source/update.bat
@@ -1,16 +1,27 @@
 @echo off
-
+echo starting to update the OpenRMM Agent
 set update_url=%1
 
 IF "%1"=="" (
     set update_url="https://github.com/OpenRMM/Agent.git"
 )
 CD C:\OpenRMM\
+echo starting git clone from update source: %update_url% 
 "C:\Program Files\Git\bin\git" clone %update_url% temp
-robocopy /IS /E /MOVE /IM "temp\source\" "Agent\"
+echo starting copy from temp
+robocopy temp\source\ Agent\ /IS /E /MOVE /IM /XF update.bat
+echo waiting 5 seconds
 timeout 5
+echo restarting service
 CD C:\OpenRMM\Agent\Py
 py OpenRMM.py update
 py OpenRMM.py restart
 ::start py OpenRMM.py debug
-pause
+echo waiting 2 seconds
+timeout 2
+echo deleting temp dir C:\OpenRMM\temp
+CD C:\OpenRMM\
+rmdir /S /Q temp
+echo Update Complete
+timeout 2
+EXIT
